99782032843409e2faf04779884c2b5b71ea2620
xubzhlin <zorre123@163.com>
2018-08-10 09:13:36 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+import sys
+from setuptools import setup, find_packages
+
+NAME = "swagger_server"
+VERSION = "1.0.0"
+
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["connexion"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Transit Server",
+    author_email="zhonglin@chinaexpresair.com",
+    url="",
+    keywords=["Swagger", "Transit Server"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    package_data={'': ['swagger/swagger.yaml']},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['swagger_server=swagger_server.__main__:main']},
+    long_description="""\
+    华夏航空FOSS中转保障系统服务接口.
+    """
+)
+
